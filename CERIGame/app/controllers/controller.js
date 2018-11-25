5ed6298f8bdcc635c81ba531a8a34f8bd2278b34
Casanova Delfeil <uapv1602054@pedago02a.univ-avignon.fr>
2018-11-25 23:18:00 +0100

    quizz sans css ni chrono ni bdd

--- CERIGame/app/controllers/controller.js
+++ CERIGame/app/controllers/controller.js
@@ -1,16 +1,110 @@
 
 function bandeau_controller($scope, session) {
 }
- 
-function quizz_controller($scope, accessDataService) {
+  
+function quizz_controller($scope, session, accessDataService) {
 	$scope.quizzs = null;
-	$scope.getQuizzes = function() {
-		accessDataService.getInfo('/quizzList', function(data) {
-			console.log("result: ", data)
-			$scope.quizzs = data;
-		});
+	$scope.searchQuizz = true;
+	$scope.questionQuizz = false;
+	$scope.showEnd = false;
+
+
+	$scope.selectedQuizz = null;
+	$scope.question = null;
+	$scope.questionAffiche = null;
+
+	$scope.debut = null;
+	$scope.nbBonneReponse = 0;
+
+	$scope.nbQuestion = 5;
+
+	$scope.score = 0;
+
+	$scope.numQuestion = -1;
+
+	$scope.difficulte = "facile";
+
+	$scope.setDifficulte = function(difficulte) {
+		$scope.difficulte = difficulte;
 	}
 
+	$scope.getQuizzes = function() {
+		if(session.getInfo('all_quizz') == null) {
+			accessDataService.getInfo('/quizzList', function(data) {
+				console.log("result: ", data)
+				$scope.quizzs = data;
+				session.setInfo('all_quizz', data);
+			});
+		} else {
+			$scope.quizzs = session.getInfo('all_quizz');
+		}
+	}
+
+	$scope.playQuizz = function(quizz) {
+		$scope.searchQuizz = false;
+		$scope.questionQuizz = true;
+
+		$scope.selectedQuizz = quizz;
+		$scope.debut = Date.now();
+		$scope.nextQuestion();
+	}
+
+	$scope.reponse = function(reponse) {
+		if(reponse == $scope.question.réponse) {
+			$scope.nbBonneReponse = $scope.nbBonneReponse + 1;
+		}
+		$scope.nextQuestion();
+	}
+
+	$scope.nextQuestion = function() {
+		$scope.numQuestion = $scope.numQuestion + 1;
+		if($scope.numQuestion >= $scope.selectedQuizz.quizz.length || $scope.numQuestion == ($scope.nbQuestion - 1)) {
+			$scope.questionQuizz = false;
+			$scope.showEnd = true;
+			$scope.endQuizz();
+		} else {
+			$scope.question = $scope.selectedQuizz.quizz[$scope.numQuestion];
+			var nbAffiche =0;
+			switch($scope.difficulte) {
+				case 'facile' :
+					nbAffiche = 2;
+					break;
+				case 'normal' : 
+					nbAffiche = 3;
+					break;
+				case 'difficile' :
+					nbAffiche = $scope.question.propositions.length;
+					break;
+			}
+			if(nbAffiche == $scope.question.propositions.length) {
+				$scope.questionAffiche = $scope.question.propositions;
+			} else {
+				$scope.questionAffiche = [];
+				var reponsePlace = false;
+				console.log("builQuestion: reponse: ", $scope.question.réponse);
+				for(var i=0; i<$scope.question.propositions.length; i++) {
+					if($scope.question.propositions[i] == $scope.question.réponse) {
+						$scope.questionAffiche.push($scope.question.propositions[i]);
+						reponsePlace = true;
+						nbAffiche--;
+					} else if(nbAffiche > 1 && !reponsePlace) {
+						$scope.questionAffiche.push($scope.question.propositions[i]);
+						nbAffiche--;
+					} else if(nbAffiche > 0 && reponsePlace) {
+						$scope.questionAffiche.push($scope.question.propositions[i]);
+						nbAffiche--;
+					}
+				}
+			}
+		}
+	}
+
+	$scope.endQuizz = function() {
+		console.log("fin: ", $scope.debut);
+		var endTime = Date.now();
+		var timeS = Math.floor((endTime - $scope.debut)/1000);
+		$scope.score = Math.round(($scope.nbBonneReponse*1398.2)/timeS);
+	}
 }
 
 function user_controller($scope, session, accessDataService) {
